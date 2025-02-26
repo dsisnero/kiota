d13594b475ba9af6c0ca3cda0172ccde13cff40a
thewahome <thewahome.cw@gmail.com>
2025-02-26 15:58:07 +0300

    remove alias

--- vscode/microsoft-kiota/webpack.config.cjs
+++ vscode/microsoft-kiota/webpack.config.cjs
@@ -31,10 +31,7 @@ const extensionConfig = {
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    extensions: ['.ts', '.js'],
-    alias: {
-      "@microsoft/kiota": path.resolve(__dirname, "node_modules/@microsoft/kiota/dist/cjs")
-    }
+    extensions: ['.ts', '.js']
   },
   module: {
     rules: [
